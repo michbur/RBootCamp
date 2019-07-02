7783b84daf86e7a3daa544df799dcd87e19b5b7d
michbur <michalburdukiewicz@gmail.com>
2019-07-02 12:39:33 +0200

    added noifs

---
+++ noifs.R
@@ -0,0 +1,14 @@
+df <- data.frame(id = paste0("S", 1L:3),
+                 sq = c("AAATGT", "TGAATTTC", "GCCCTAT"),
+                 s = c("+", "-", "+"), stringsAsFactors = FALSE)
+
+get_sq <- function(x) {
+  browser()
+  fun_list <- list(`+` = identity, `-` = rev)
+  chosen_funs <- setNames(fun_list[df[["s"]]], df[["id"]])
+  splitted_seqs <- setNames(strsplit(df[["sq"]], ""), df[["id"]])
+  lapply(df[["id"]], function(i) chosen_funs[[i]](splitted_seqs[[i]]))
+  
+}
+
+get_sq(df)
