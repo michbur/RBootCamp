ba790da3478da8f02569de191d2e2d395cdf1b41
michbur <michalburdukiewicz@gmail.com>
2019-07-02 15:54:57 +0200

    added dplyr intro

--- dplyr-intro.R
+++ dplyr-intro.R
@@ -16,3 +16,32 @@ x %>%
                                           "H", "I", "K", "L", "M", "N", 
                                           "P", "Q", "R", "S", "T", "V", "W", "Y")) %>% 
            table)
+
+# dplyr examples
+
+cbind(iris, sepal_square = iris[["Sepal.Length"]]^2)
+
+mutate(iris, sepal_square = Sepal.Length^2)
+
+group_by(iris, Species) %>% 
+  summarise(n_species = length(Species))
+
+norm01 <- function(x)
+  (x - min(x))/(max(x) - min(x))
+
+group_by(iris, Species) %>% 
+  mutate(norm_petal_length = norm01(Petal.Length))
+
+mutate(iris, norm_petal_length = norm01(Petal.Length))
+
+group_by(iris, Species) %>% 
+  mutate(norm_petal_length = norm01(Petal.Length)) %>% 
+  summarise(mnpl = mean(norm_petal_length)) %>% 
+  pull(mnpl)
+
+group_by(iris, Species) %>% 
+  filter(Species != "virginica") %>% 
+  mutate(norm_petal_length = norm01(Petal.Length)) %>% 
+  summarise(mnpl = mean(norm_petal_length)) %>% 
+  select(mnpl)
+
