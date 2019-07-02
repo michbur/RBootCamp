cec68c7081cbe8c75d6274a1921131283cb135e7
michbur <michalburdukiewicz@gmail.com>
2019-07-02 12:40:38 +0200

    removed browser clause

--- noifs.R
+++ noifs.R
@@ -3,12 +3,10 @@ df <- data.frame(id = paste0("S", 1L:3),
                  s = c("+", "-", "+"), stringsAsFactors = FALSE)
 
 get_sq <- function(x) {
-  browser()
   fun_list <- list(`+` = identity, `-` = rev)
   chosen_funs <- setNames(fun_list[df[["s"]]], df[["id"]])
   splitted_seqs <- setNames(strsplit(df[["sq"]], ""), df[["id"]])
   lapply(df[["id"]], function(i) chosen_funs[[i]](splitted_seqs[[i]]))
-  
 }
 
 get_sq(df)
