6daeef5305d69b896b777818671c2d229e5d717b
michbur <michalburdukiewicz@gmail.com>
2019-07-02 15:39:07 +0200

    pipe example

--- dplyr-intro.R
+++ dplyr-intro.R
@@ -1,6 +1,18 @@
 x <- c("MYGGP", "GGGHMIL")
 splitted_x <- strsplit(x, "")
-factor_x <- lapply(splitted_x, factor, levels = c("A", "C", "D", "E", "F", "G", 
-                                                  "H", "I", "K", "L", "M", "N", 
-                                                  "P", "Q", "R", "S", "T", "V", "W", "Y"))
-lapply(factor_x, table)
+lapply(splitted_x, function(i) table(factor(i, levels = c("A", "C", "D", "E", "F", "G", 
+                                                          "H", "I", "K", "L", "M", "N", 
+                                                          "P", "Q", "R", "S", "T", "V", "W", "Y"))))
+
+res <- lapply(splitted_x <- strsplit(x, ""), function(i) table(factor(i, levels = c("A", "C", "D", "E", "F", "G", 
+                                                          "H", "I", "K", "L", "M", "N", 
+                                                          "P", "Q", "R", "S", "T", "V", "W", "Y"))))
+
+library(dplyr)
+
+x %>% 
+  strsplit("") %>% 
+  lapply(function(i) factor(i, levels = c("A", "C", "D", "E", "F", "G", 
+                                          "H", "I", "K", "L", "M", "N", 
+                                          "P", "Q", "R", "S", "T", "V", "W", "Y")) %>% 
+           table)
