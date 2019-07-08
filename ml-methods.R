faf94ab0b652db8da9cbb6947eaf3d19f908617a
michbur <michalburdukiewicz@gmail.com>
2019-07-08 10:51:44 +0200

    added ml-methods

---
+++ ml-methods.R
@@ -0,0 +1,11 @@
+library(rpart)
+library(rpart.plot)
+library(ggplot2)
+library(dplyr)
+
+iris2 <- iris[c("Sepal.Length", "Petal.Length", "Species")] %>% 
+  mutate(Species = Species == "setosa")
+  
+
+ggplot(iris2, aes(x = Sepal.Length, y = Petal.Length, color = Species)) +
+  geom_point()
