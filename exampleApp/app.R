c8c842e3111a846f3763d4457c4f7343f732f7fd
michbur <michalburdukiewicz@gmail.com>
2019-07-10 15:32:20 +0200

    added plot

--- exampleApp/app.R
+++ exampleApp/app.R
@@ -1,37 +1,49 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(ggplot2)
+library(dplyr)
 
 ui <- fluidPage(
-
+    
     titlePanel("SuperShinyApp"),
-
+    
     sidebarLayout(
         sidebarPanel(
             fileInput("fasta_file", "Please provide a valid .fasta file")
         ),
-
+        
         # Show a plot of the generated distribution
         mainPanel(
-           textOutput("fasta_file_content")
+            plotOutput("aa_composition"),
+            verbatimTextOutput("fasta_file_content")
         )
     )
 )
 
 server <- function(input, output) {
     
-    output[["fasta_file_content"]] <- renderText({
-        browser()
+    fasta_content <- reactive({
+        readLines(input[["fasta_file"]][["datapath"]])[-1]
     })
-
-
+    
+    output[["fasta_file_content"]] <- renderPrint({
+        # if(!is.null(input[["fasta_file"]]))
+        #     browser()
+        
+        cat(fasta_content(), sep = "\n")
+    })
+    
+    output[["aa_composition"]] <- renderPlot({
+        
+        fasta_content() %>% 
+            strsplit("") %>% 
+            unlist %>% 
+            table(aa = .) %>% 
+            data.frame %>% 
+            ggplot(aes(x = aa, y = Freq)) +
+            geom_col()
+    })
+    
+    
 }
 
 shinyApp(ui = ui, server = server)
